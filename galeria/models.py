33b9abd67edb4b7a089861ee0373229ab6ba1e67
Bruno Souza <brunosouza.cup@gmail.com>
2023-12-01 16:30:55 -0300

    hotfix

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# null=False --> Não pode ser nula
+class Fotografia(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    legenda = models.CharField(max_length=150, null=False, blank=False)
+    descricao = models.TextField(null=False, blank=False)
+    foto = models.CharField(max_length=100, null=False, blank=False)
+    
+    # Retorna a string nome
+    def __str__(self):
+        return f"Fotografia {self.nome}"
