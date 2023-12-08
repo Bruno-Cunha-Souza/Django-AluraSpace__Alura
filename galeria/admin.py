24ac29d075afa8b2df2acf8e9ee1d5c462e82636
Bruno Souza <brunosouza.cup@gmail.com>
2023-12-07 23:47:11 -0300

    add filtro de busca

--- galeria/admin.py
+++ galeria/admin.py
@@ -5,8 +5,10 @@ from galeria.models import Fotografia
 class ListandoFotografia(admin.ModelAdmin):
     list_display = ("id", "nome", "legenda")
     list_display_links = ("id", "nome")
+    #Pesquisar pelo nome
     search_fields = ("nome",)
-
-
+    #Filtrar por categoria
+    list_filter = ("categoria",)
+    
 # registrando o db Fotografia no admin do django
 admin.site.register(Fotografia, ListandoFotografia)
