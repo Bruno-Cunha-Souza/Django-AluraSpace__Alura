a34205563583184a4d708c31e3f50d07c8ad74d8
Bruno Souza <brunosouza.cup@gmail.com>
2023-12-07 23:40:39 -0300

    updates

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from galeria.models import Fotografia
+
+class ListandoFotografia(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda")
+    list_display_links = ("id", "nome")
+    search_fields = ("nome",)
+
+
+# registrando o db Fotografia no admin do django
+admin.site.register(Fotografia, ListandoFotografia)
